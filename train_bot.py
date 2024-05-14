3b30498111adee30e3c55c5eb9a89bc62ec4194e
IvannaTorres <119988894+IvannaTorres@users.noreply.github.com>
2024-05-14 13:37:07 -0500

    Add files via upload

--- train_bot.py
+++ train_bot.py
@@ -1,31 +1,32 @@
-#Biblioteca de preprocesamiento de datos de texto
 import nltk
-
 from nltk.stem import PorterStemmer
 stemmer = PorterStemmer()
-
 import json
 import pickle
 import numpy as np
-
 words=[]
 classes = []
 word_tags_list = []
 ignore_words = ['?', '!',',','.', "'s", "'m"]
-train_data_file = open('/content/C137Boot/intents.json').read()
+train_data_file = open('/content/c137_1/intents.json').read()
 intents = json.loads(train_data_file)
-
 # Función para añadir palabras raíz (stem words)
 
-    
+
+
+
+
         # Agregar todas las palabras de los patrones a una lista
+        
+
 
 
         # Agregar todas las etiquetas a la lista de clases
- 
+       
+
+
+
 
 
 # Crear un corpus de palabras para el chatbot
 
-
-     
